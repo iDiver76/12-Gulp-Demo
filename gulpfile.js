09215d21a6ab7380c6f2d36ae5f91441bcaea459
Thomas Jaggi <thomas.jaggi@unic.com>
2015-08-19 13:29:45 +0200

    Add sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass');
 
 gulp.task('js', function() {
 	return gulp.src('source/*.js')
@@ -12,6 +13,13 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('build/'));
 });
 
+gulp.task('css', function() {
+	return gulp.src('source/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build/'));
+});
+
 gulp.task('default', function() {
 	gulp.watch('source/*.js', ['js']);
+	gulp.watch('source/*.scss', ['css']);
 });
