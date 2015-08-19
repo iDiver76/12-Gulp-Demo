456da80f527483d5eb8745186ec7fac378d9f20d
Thomas Jaggi <thomas.jaggi@unic.com>
2015-08-19 13:27:51 +0200

    Add bootstrap demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass');
 
 gulp.task('js', function() {
-	return gulp.src('source/*.js')
+	return gulp.src([
+			'node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
+			'source/*.js'
+		])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(concat('main.js'))
@@ -12,6 +16,13 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('build/'));
 });
 
+gulp.task('css', function() {
+	return gulp.src('source/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build/'));
+});
+
 gulp.task('default', function() {
 	gulp.watch('source/*.js', ['js']);
+	gulp.watch('source/*.scss', ['css']);
 });
