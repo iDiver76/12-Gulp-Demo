583c80bcc4e8e246eff1f171d7785e8858b97c51
Thomas Jaggi <thomas.jaggi@unic.com>
2016-08-17 10:02:24 +0200

    Add postCSS and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	postCSS = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer');
 
 gulp.task('js', function() {
 	return gulp.src('source/*.js')
@@ -16,6 +18,11 @@ gulp.task('js', function() {
 gulp.task('css', function() {
 	return gulp.src('source/*.scss')
 		.pipe(sass())
+		.pipe(postCSS([
+			autoprefixer({
+				browsers: ['last 10 versions']
+			})
+		]))
 		.pipe(gulp.dest('build/'));
 });
 
