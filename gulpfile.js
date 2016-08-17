5af72d3189abbe5bf1e7d4f1b25487f58777b403
Thomas Jaggi <thomas.jaggi@unic.com>
2016-08-17 13:25:45 +0200

    Remove JS task and files, run sprite and CSS task in sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
 	sass = require('gulp-sass'),
 	spritesmith = require('gulp.spritesmith'),
-	merge = require('merge-stream');
-
-gulp.task('js', function() {
-	return gulp.src('source/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('build/'));
-});
+	merge = require('merge-stream'),
+	runSequence = require('run-sequence');
 
 gulp.task('css', function() {
 	return gulp.src('source/*.scss')
@@ -39,7 +28,6 @@ gulp.task('sprite', function() {
 	return merge(streams.img, streams.css);
 });
 
-gulp.task('default', function() {
-	gulp.watch('source/*.js', ['js']);
-	gulp.watch('source/*.scss', ['css']);
+gulp.task('default', function(cb) {
+	runSequence('sprite', 'css', cb);
 });
