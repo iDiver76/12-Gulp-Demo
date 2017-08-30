4a11e49c7f2ff2d8d50474ab976e5dfdb4f71ca2
David Janssen <dave.janssen@me.com>
2017-08-30 20:59:02 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('js', function() {
 	return gulp.src('source/*.js')
@@ -12,6 +15,18 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('build/'));
 });
 
+gulp.task('sass', function() {
+	return gulp.src('source/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/css'));
+});
+
 gulp.task('default', function() {
-	gulp.watch('source/*.js', ['js']);
+	gulp.watch(['source/*.js','source/scss/*.scss'], ['js','sass']);
 });
